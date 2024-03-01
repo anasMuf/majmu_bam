82cf474410bbaee90f8e3aff23476414d4d31c0c
anasMuf <anas.muhammadakbar@gmail.com>
2024-03-02 03:48:29 +0700

    update asmaulhusna dan test disabled zoom browser

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,8 @@
 
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    {{-- <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /> --}}
     <meta name="description" content="Aplikasi Majmu' BAM dari Juruladen untuk Keluarga BAM">
     <meta name="keywords" content="majmu bam">
     <meta name="author" content="juruladen bam">
